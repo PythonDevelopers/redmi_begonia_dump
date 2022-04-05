009f7e8382f7ef7a5f781cc71cda0f6c92f879ce
HyperNotAryanX97 <ahirearyan2016@gmail.com>
2022-04-05 10:35:42 +0000

    Add extras for begonia-user 10 QP1A.190711.020 V12.0.8.0.QGGMIXM release-keys
    Signed-off-by: HyperNotAryanX97 <ahirearyan2016@gmail.com>

---
+++ README.md
@@ -0,0 +1,21 @@
+## begonia-user 10 QP1A.190711.020 V12.0.8.0.QGGMIXM release-keys
+- Manufacturer: xiaomi
+- Platform: mt6785
+- Codename: begonia
+- Brand: Redmi
+- Flavor: begonia-user
+- Release Version: 11
+- Id: QP1A.190711.020
+- Incremental: V12.0.8.0.QGGMIXM
+- Tags: test-keys
+- CPU Abilist: arm64-v8a,armeabi-v7a,armeabi
+- A/B Device: false
+- Locale: en-US
+- Screen Density: undefined
+- Fingerprint: Redmi/begonia/begonia:10/QP1A.190711.020/V12.0.8.0.QGGMIXM:user/release-keys
+- OTA version: 
+- Branch: begonia-user-10-QP1A.190711.020-V12.0.8.0.QGGMIXM-release-keys
+- Repo: redmi_begonia_dump
+
+
+>Dumped by [Phoenix Firmware Dumper](https://github.com/DroidDumps/phoenix_firmware_dumper)
---
+++ join_split_files.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cat system/system/priv-app/MiuiCamera/MiuiCamera.apk.* 2>/dev/null >> system/system/priv-app/MiuiCamera/MiuiCamera.apk
+rm -f system/system/priv-app/MiuiCamera/MiuiCamera.apk.* 2>/dev/null
+cat system/system/app/MiuiVideoPlayer/oat/arm64/MiuiVideoPlayer.odex.* 2>/dev/null >> system/system/app/MiuiVideoPlayer/oat/arm64/MiuiVideoPlayer.odex
+rm -f system/system/app/MiuiVideoPlayer/oat/arm64/MiuiVideoPlayer.odex.* 2>/dev/null
+cat system/system/app/MiuiVideoPlayer/MiuiVideoPlayer.apk.* 2>/dev/null >> system/system/app/MiuiVideoPlayer/MiuiVideoPlayer.apk
+rm -f system/system/app/MiuiVideoPlayer/MiuiVideoPlayer.apk.* 2>/dev/null
+cat system/system/product/app/webview/webview.apk.* 2>/dev/null >> system/system/product/app/webview/webview.apk
+rm -f system/system/product/app/webview/webview.apk.* 2>/dev/null
+cat boot.img.* 2>/dev/null >> boot.img
+rm -f boot.img.* 2>/dev/null
+cat recovery.img.* 2>/dev/null >> recovery.img
+rm -f recovery.img.* 2>/dev/null
